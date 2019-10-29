2b0efc29d0171f683bb86aae3d7e8765ee9574e3
Nella <nella.pihlajaniemi>
2019-10-29 17:15:41 +0200

    koodia devnella branchiin

--- projekti/src/Main.java
+++ projekti/src/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println("merge exercise");
         System.out.println("More important stuff!");
         System.out.println("Merge conflict");
+        System.out.println("All this code was made in devnella branch");
     }
 }
